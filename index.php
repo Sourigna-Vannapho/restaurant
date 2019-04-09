d63dac37fdfd428ab23b9b086d24dde845bc89d4
Sourigna <sourignavannapho@gmail.com>
2019-04-10 00:32:29 +0200

    routing and controller done for homepage

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+require('controller/controller.php');
+
+try{
+	if (isset($_GET['action'])) {
+		if ($_GET['action'] == 'home'){
+			homepage();
+		}
+	else{
+		homepage();
+	}
+}
+catch(Exception $e){
+	$errorMsg = $e->getMessage();
+}
